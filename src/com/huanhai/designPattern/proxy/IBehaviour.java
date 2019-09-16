8dc95155a956ea2a5537800c11274446cc6a5479
boqin <842693026@qq.con>
2019-09-16 17:07:41 +0800

    feat:代理模式提交

---
+++ src/com/huanhai/designPattern/proxy/IBehaviour.java
@@ -0,0 +1,8 @@
+package com.huanhai.designPattern.proxy;
+
+public interface IBehaviour {
+    /**
+     * 下单
+     */
+    void order();
+}
---
+++ src/com/huanhai/designPattern/proxy/ProxyBoot.java
@@ -0,0 +1,14 @@
+package com.huanhai.designPattern.proxy;
+
+/**
+ * @version 1.0
+ * @Description: TO DO
+ * @Author: 覃波
+ * @Date: 2019/9/16
+ */
+public class ProxyBoot {
+    public static void main(String[] args) {
+        Proxy p=new Proxy();
+        p.order();
+    }
+}
