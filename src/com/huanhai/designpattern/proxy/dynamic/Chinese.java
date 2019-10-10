821e0e333d235f6f693e4d0e226e3dd99f881394
boqin <842693026@qq.con>
2019-10-10 16:22:11 +0800

    feat:动态代理

---
+++ src/com/huanhai/designpattern/proxy/dynamic/Chinese.java
@@ -0,0 +1,14 @@
+package com.huanhai.designpattern.proxy.dynamic;
+
+/**
+ * @version 1.0
+ * @Description: TO DO
+ * @Author: 覃波
+ * @Date: 2019/10/10
+ */
+public class Chinese implements People {
+    @Override
+    public void sayHello() {
+        System.out.println("Chinese say hello.");
+    }
+}
