af313961322ec9b12cbf9d3ad4ca75bad755d324
boqin <842693026@qq.con>
2019-10-14 16:23:12 +0800

    fix:IO修改

---
+++ src/com/huanhai/io/simpleio/Client.java
@@ -0,0 +1,8 @@
+package com.huanhai.io.simpleio;
+
+
+public class Client {
+    public static void main(String[] args) {
+        new FileInput().readFileByChar("D:\\a.txt");
+    }
+}
--- src/com/huanhai/io/simpleio/FileInput.java
+++ src/com/huanhai/io/simpleio/FileInput.java
@@ -3,38 +3,60 @@ package com.huanhai.io.simpleio;
 import java.io.*;
 
 /**
+ * 文件处理类
+ *
+ * @author 覃波
  * @version 1.0
- * @Description: 读取
- * @Author: 覃波
- * @Date: 2019/9/16
+ * @date 2019/9/16
  */
 public class FileInput {
-  public static void readFile(String path) {
-      InputStream in=null;
-      InputStreamReader fr=null;
-      BufferedReader bt=null;
-      try{
-          //获取文件输入流
-           in =new FileInputStream(path);
-          //缓冲流
-           fr =new InputStreamReader(in);
-           bt=new BufferedReader(fr);
-          String ctx=null;
-          while ((ctx =bt.readLine())!= null){
-              System.out.println(ctx);
-          }
-      }catch (Exception e){
-          e.printStackTrace();
-      }finally {
-          try {
-              bt.close();
-              fr.close();
-              in.close();
-          } catch (IOException e) {
-              e.printStackTrace();
-          }
+    /**
+     * 读取文件
+     *
+     * @param path 文件路径
+     * @return 无
+     * @author 覃波
+     * @date 2019/10/14
+     */
+    public void readFileByChar(String path) {
+        InputStream in = null;
+        InputStreamReader fr = null;
+        BufferedReader bt = null;
+        try {
+            //获取文件输入流
+            in = new FileInputStream(path);
+            //缓冲流
+            fr = new InputStreamReader(in);
+            bt = new BufferedReader(fr);
+            String ctx = null;
+            while ((ctx = bt.readLine()) != null) {
+                System.out.println(ctx);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bt.close();
+                fr.close();
+                in.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-      }
+        }
 
-  }
+    }
+
+    /**
+     * 写入文件
+     *
+     * @param path 输出文件路径
+     * @return 无
+     * @author 覃波
+     * @date 2019/10/14
+     */
+    public void writeFileByChar(String path) {
+        OutputStream outputStream = null;
+
+    }
 }
