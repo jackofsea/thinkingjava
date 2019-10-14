4be6553aa7bd9380b03938f7698a0f8e1985df61
boqin <842693026@qq.con>
2019-10-14 11:22:55 +0800

    feat:中介者者模式提交

---
+++ src/com/huanhai/designpattern/Mediator/Colleague.java
@@ -0,0 +1,24 @@
+package com.huanhai.designpattern.Mediator;
+
+/**
+ * @version 1.0
+ * @Description: 抽象同事类
+ * @Author: 覃波
+ * @Date: 2019/10/14
+ */
+public abstract class Colleague {
+    //持有一个调停者对象
+    private Mediator mediator;
+    /**
+     * 构造函数
+     */
+    public Colleague(Mediator mediator){
+        this.mediator = mediator;
+    }
+    /**
+     * 获取当前同事类对应的调停者对象
+     */
+    public Mediator getMediator() {
+        return mediator;
+    }
+}
