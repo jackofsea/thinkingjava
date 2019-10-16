91987e25014e12d9fb0fd4d06e56c5e3ee679294
boqin <842693026@qq.con>
2019-10-16 17:20:34 +0800

    fix:IO修改

--- src/com/huanhai/io/simpleio/Client.java
+++ src/com/huanhai/io/simpleio/Client.java
@@ -1,8 +1,23 @@
 package com.huanhai.io.simpleio;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Client {
-    public static void main(String[] args) {
-        new FileInput().readFileByChar("D:\\a.txt");
+    public static void main(String[] args) throws IOException {
+        InputStream fs = null;
+
+        try {
+             fs=new FileInputStream("D:\\a.txt");
+            new FileInput().writeFileByChar(fs,"E:\\b.txt");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            fs.close();
+        }
+
+
     }
 }
--- src/com/huanhai/io/simpleio/FileInput.java
+++ src/com/huanhai/io/simpleio/FileInput.java
@@ -48,15 +48,74 @@ public class FileInput {
     }
 
     /**
-     * 写入文件
+     * 写入文本文件
      *
      * @param path 输出文件路径
      * @return 无
+     * @throws NullPointerException
      * @author 覃波
      * @date 2019/10/14
      */
-    public void writeFileByChar(String path) {
-        OutputStream outputStream = null;
+    public void writeFileByChar(InputStream in, String path) {
+        if (in == null) {
+            throw new NullPointerException();
+        }
+        InputStreamReader fr = null;
+        BufferedReader bt = null;
+        OutputStream outputStream =null;
+        OutputStreamWriter osw=null;
+        BufferedWriter bw=null;
+        try {
+            //缓冲流
+            fr = new InputStreamReader(in);
+            bt = new BufferedReader(fr);
+            outputStream = new FileOutputStream(path);
+            osw = new OutputStreamWriter(outputStream);
+            bw = new BufferedWriter(osw);
+//            byte[] b = new byte[1024];
+            String tmp;
+            while ((tmp=bt.readLine()) != null){
+                bw.write(tmp);
+            }
 
+        } catch (Exception e) {
+
+        } finally {
+            close(bt);
+            close(fr);
+            close(in);
+            close(bw);
+            close(osw);
+            close(outputStream);
+        }
+
+    }
+    private void close(InputStream in){
+        try {
+            in.close();
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+    private void close(Reader reader){
+        try {
+            reader.close();
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+    private void close(OutputStream out){
+        try {
+            out.close();
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+    private void close(Writer writer){
+        try {
+            writer.close();
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
