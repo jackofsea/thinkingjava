5c0ffde5a5517f7eb771ac8da5f1ccaf49993662
jack <842693026@qq.com>
2020-05-07 23:45:01 +0800

    tran: Long对象的创建

---
+++ src/com/huanhai/test/InitTest.java
@@ -0,0 +1,14 @@
+package com.huanhai.test;
+
+/**
+ * @Description 初始化顺序测试
+ * @Author 覃波
+ * @Date 2020/4/21 8:42
+ * @Version 1.0
+ **/
+public class InitTest {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/com/huanhai/thinkjava/base/LongTest.java
@@ -0,0 +1,18 @@
+package com.huanhai.thinkjava.base;
+
+/**
+ * @Description Long的API学习和熟悉
+ * @Author 覃波
+ * @Date 2020/4/20 12:20
+ * @Version 1.0
+ **/
+public class LongTest {
+    public static void main(String[] args) {
+
+    }
+    //Long对象的创建
+    public static void valueOf(){
+        //等同于Long L1=22
+      Long L1=Long.valueOf(22);
+    }
+}
