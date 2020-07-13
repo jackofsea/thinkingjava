0f19189ccc947b69f503dbe5651bfcf063d67b6b
jack <842693026@qq.com>
2020-07-13 15:41:45 +0800

    feat: Long的一些函数

--- src/com/huanhai/thinkjava/base/LongTest.java
+++ src/com/huanhai/thinkjava/base/LongTest.java
@@ -10,6 +10,12 @@ public class LongTest {
     public static void main(String[] args) {
         //Long对象创建和进制转换
         valueOf();
+        //打印进制字符
+        toRadixString();
+        //long的运算
+        operation();
+        //long的一些bit操作
+        bitOperation();
     }
 
     //Long对象的静态创建
@@ -26,10 +32,46 @@ public class LongTest {
         System.out.println("L1:"+L1+"L2:"+L2+"L3:"+L3+"L4:"+L4);
         //编译后调用队长xxValue()方法，下面代码等同于long long1=L1.intValue(),也称自动拆箱
         long long1=L1;
-        //进制转换，将对应的进制直接转为10进制的long
+        //进制转换为Long，将对应的进制直接转为10进制的long,参数为解析的进制
         Long radix8=Long.valueOf("234",8);
         Long radix2=Long.valueOf("1011",2);
-        Long radix22=Long.parseLong("1111",2);
-        System.out.println("radix8:"+radix8+" "+"radix2:"+radix2+" "+"radix22:"+radix22);
+        System.out.println("radix8:"+radix8+" "+"radix2:"+radix2);
+        //字符串转换为Long,不带参数默认按10进制转换
+        Long radix2p=Long.parseLong("1111",2);
+        Long radix10=Long.parseLong("11123");
+        System.out.println("radix2p:"+radix2p+" "+"radix10:"+radix10);
     }
+
+    //Long转换为字符串
+    public static void toRadixString() {
+        //转换为二进制
+        System.out.println(Long.toBinaryString(23245411111L));
+        //转换为八进制
+        System.out.println(Long.toOctalString(23245411111L));
+        //转换为十六进制
+        System.out.println(Long.toHexString(23245411111L));
+        //转换为字符串
+        System.out.println(new Long(34222219));
+        //转换为无符号字符串
+        System.out.println(Long.toUnsignedString(-3));
+    }
+
+    //Long的一些运算
+    public static void operation() {
+       //比较大小，底层用的Math类
+        System.out.println(Long.max(2323,4444));
+        System.out.println(Long.min(2323,4444));
+        //求和
+        System.out.println(Long.sum(2323,4444));
+    }
+
+    //Long的一些bit操作
+    public static void bitOperation() {
+        //signum,取得数值的符号函数，-1是负数，0是0,1是整数
+        System.out.println(Long.signum(-32323));
+
+
+    }
+
+
 }
