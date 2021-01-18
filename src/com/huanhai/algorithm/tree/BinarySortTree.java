f2429cc278365c85784b6b24c67436f9a8bd66f9
jack <842693026@qq.com>
2021-01-18 17:21:11 +0800

    learn:二叉排序树的初步构建

--- src/com/huanhai/algorithm/tree/BinarySortTree.java
+++ src/com/huanhai/algorithm/tree/BinarySortTree.java
@@ -1,14 +1,33 @@
 package com.huanhai.algorithm.tree;
 
 /**
- *
  * 二叉查找树（二叉排序树）
+ *
+ * 二叉排序树又称二叉排序树。它或者是一个空树，或者是一个具有下列性质的二叉树：
+ *  1)若它的左子树不空，则左子树上所有节点的值均小于它的根结构的值；
+ *  2)若它的右子树不空，则右子树上所有结点的值均大于它的根节点的值；
+ *  3)它的左、右子树也分别是二叉排序树。
+ *
+ *
  *
  * @author 覃波
  * @version 1.0
  * @date 2021-01-15 15:42
  **/
 public class BinarySortTree {
+    public static void main(String[] args) {
+        int[] val = new int[]{55, 23, 3, 23, 43, 45, 67, 23, 67};
+
+    }
+
+    TreeNode createBinarySortTree(int[] val) {
+        if (val == null || val.length < 1) {
+            return null;
+        }
+        TreeNode root = new TreeNode(val[0], null, null);
+        return null;
+
+    }
 
 
     /**
@@ -58,10 +77,10 @@ public class BinarySortTree {
 
         @Override
         public int compareTo(TreeNode o) {
-            if(value == o.value){
+            if (value == o.value) {
                 return 0;
             }
-            return value > o.value ? -1 :1;
+            return value > o.value ? -1 : 1;
         }
     }
 }
