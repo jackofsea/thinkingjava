851e94ff47427f197e335e3c33ec53f0cc8e4290
842693026@qq.com <shang915->
2021-02-28 22:27:46 +0800

    feat:基于数组和链表的栈实现

--- src/com/huanhai/algorithm/stack/ArrayStack.java
+++ src/com/huanhai/algorithm/stack/ArrayStack.java
@@ -1,11 +1,59 @@
 package com.huanhai.algorithm.stack;
 
 /**
- *  数组实现栈
+ * 数组实现栈
  *
  * @author 覃波
  * @version 1.0
  * @date 2021-02-26 11:48
  **/
-public class ArrayStack {
+public class ArrayStack<T> {
+
+    private Object[] objects;
+    private int size;
+
+    public ArrayStack() {
+        objects = new Object[16];
+    }
+
+    public ArrayStack(int deep) {
+        objects = new Object[deep];
+    }
+
+    public void push(T o) {
+        objects[size++] = o;
+    }
+
+    public T pop() {
+        Object o = objects[--size];
+        objects[size] = null;
+        return (T) o;
+    }
+
+    /**
+     * 查看栈顶元素
+     *
+     * @return
+     */
+    public T peek() {
+        Object o = objects[size - 1];
+        return (T) o;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public static void main(String[] args) {
+        ArrayStack<String> stack = new ArrayStack<>();
+        stack.push("n你是");
+        stack.push("n你2是");
+        stack.push("n你是3");
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty() + " " + stack.size);
+    }
+
 }
+
--- src/com/huanhai/algorithm/stack/LinkStack.java
+++ src/com/huanhai/algorithm/stack/LinkStack.java
@@ -1,11 +1,76 @@
 package com.huanhai.algorithm.stack;
 
 /**
- *  链表实现栈
+ * 链表实现栈
  *
  * @author 覃波
  * @version 1.0
  * @date 2021-02-26 11:48
  **/
-public class LinkStack {
+public class LinkStack<T> {
+    private Node head;
+    private Node tail;
+
+    public void push(T o) {
+        Node node = new Node(o);
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    public T peek() {
+        return (T) tail.object;
+    }
+
+    /**
+     * 出栈
+     *
+     * @return
+     */
+    public T pop() {
+        if (head == tail) {
+            head = null;
+            tail = null;
+        }
+        Node node = tail;
+        Node pre = null;
+        Node tmp = head;
+        while (tmp != tail) {
+            pre = tmp;
+            tmp = tmp.next;
+        }
+        tail = pre;
+        return node.object;
+    }
+
+    class Node {
+        private T object;
+        private Node next;
+
+        public Node(T o) {
+            object = o;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkStack<String> linkStack = new LinkStack<>();
+        linkStack.push("212");
+        linkStack.push("23111");
+        linkStack.push("傻逼");
+        linkStack.push("二逼");
+        System.out.println(linkStack.pop());
+        System.out.println(linkStack.peek());
+    }
 }
