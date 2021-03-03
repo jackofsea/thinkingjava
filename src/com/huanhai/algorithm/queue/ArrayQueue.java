acba8bd84e15cb222d8001b15913962cb8e316d0
jack <842693026@qq.com>
2021-03-03 17:40:46 +0800

    fix:优化队列

--- src/com/huanhai/algorithm/queue/ArrayQueue.java
+++ src/com/huanhai/algorithm/queue/ArrayQueue.java
@@ -36,12 +36,22 @@ public class ArrayQueue<E> {
      * 入队
      * @param e
      */
-    public void push(E e) {
+    public void add(E e) {
         if(isFull()){
             return;
         }
+        //队尾已经达到,向前移动元素
+        if(tail >= elements.length){
+            int offer= head;
+             for (int i=0;i<size;i++){
+               elements[i]=elements[offer+i];
+             }
+             head=0;
+             tail-=offer;
+        }
         elements[tail++] = e;
         size++;
+
     }
 
     /**
@@ -49,7 +59,7 @@ public class ArrayQueue<E> {
      *
      * @return
      */
-    public E pop() {
+    public E poll() {
         if (isEmpty()) {
             return null;
         }
@@ -57,6 +67,13 @@ public class ArrayQueue<E> {
         elements[head-1] = null;
         size--;
         return e;
+    }
+
+    /**
+     * 查看队头的元素
+     */
+    public E peek() {
+        return tail == elements.length? null:(E) elements[head];
     }
 
     public boolean isEmpty() {
@@ -74,20 +91,25 @@ public class ArrayQueue<E> {
     public static void main(String[] args) {
         ArrayQueue<String> arrayQueue=new ArrayQueue<>(5);
         System.out.println(arrayQueue.isEmpty());
-        arrayQueue.push("nishi");
-        arrayQueue.push("ni43shi");
-        arrayQueue.push("ni34shi");
-        arrayQueue.push("ni43shi");
-        arrayQueue.push("nis43hi");
+        arrayQueue.add("nishi");
+        arrayQueue.add("ni43shi");
+        arrayQueue.add("ni34shi");
+        arrayQueue.poll();
+        arrayQueue.add("ni43shi");
+        arrayQueue.add("nis43hi");
+        arrayQueue.add("nis43hi");
+        arrayQueue.add("nis43hi");
         System.out.println(arrayQueue.isEmpty() + " "+arrayQueue.isFull());
-        arrayQueue.pop();
+        arrayQueue.poll();
         System.out.println(arrayQueue.isEmpty() + " "+arrayQueue.isFull());
-        arrayQueue.pop();
-        arrayQueue.pop();
-        arrayQueue.pop();
-        arrayQueue.pop();
-        arrayQueue.pop();
+        arrayQueue.poll();
+        arrayQueue.poll();
+        arrayQueue.poll();
+        arrayQueue.poll();
+        arrayQueue.poll();
         System.out.println(arrayQueue.isEmpty() + " "+arrayQueue.isFull());
+
+        System.out.println(arrayQueue.peek());
     }
 
 
