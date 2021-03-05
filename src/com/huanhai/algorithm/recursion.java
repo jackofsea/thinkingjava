e85f3e57572c79ec893ee678860f13d97fcf5a8f
842693026@qq.com <shang915->
2021-03-06 00:08:33 +0800

    feat: 尾递归实现

--- src/com/huanhai/algorithm/recursion.java
+++ src/com/huanhai/algorithm/recursion.java
@@ -10,7 +10,10 @@ package com.huanhai.algorithm;
 public class recursion {
 
     public static void main(String[] args) {
-        System.out.println(fibon(100));
+        System.out.println(fibon(11));
+        System.out.println(fibon(11,1,1));
+        System.out.println(factorial(10));
+        System.out.println(factorial(10,1));
     }
 
     /**
@@ -25,6 +28,47 @@ public class recursion {
         }
         return fibon(i-1)+fibon(i-2);
     }
+
+
+    /**
+     * 递归求解斐波拉契数列， 1、1、2、3、5、8
+     * 尾递归
+     * @param i
+     * @return
+     */
+    public static int fibon(int i,int pre,int res){
+        if(i <= 2){
+            return res;
+        }
+        return fibon(i-1,res,pre+res);
+    }
+
+    /**
+     * 阶乘
+     *
+     * @param n
+     * @return
+     */
+    public static int factorial(int n){
+        if(n <= 1){
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+
+    /**
+     * 阶乘,尾递归
+     *
+     * @param n
+     * @return
+     */
+    public static int factorial(int n,int res){
+        if(n <= 1){
+            return res;
+        }
+        return factorial(n-1,res*n);
+    }
+
 
 
 }
